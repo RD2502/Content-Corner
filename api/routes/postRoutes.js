80175e0015265f8e2ce6e74ac7697df825c0ffbd
Roshan Das <91432324+RD2502@users.noreply.github.com>
2023-08-02 01:49:35 +0530

    Update postRoutes.js

--- api/routes/postRoutes.js
+++ api/routes/postRoutes.js
@@ -12,7 +12,7 @@ router.get('/post/:id', showSinglePost);
 router.delete('/delete/post/:id', isAuthenticated, deletePost);
 router.put('/update/post/:id', isAuthenticated, updatePost);
 router.put('/comment/post/:id', isAuthenticated, addComment);
-router.put('/addlike/post/:id', addLike);
-router.put('/removelike/post/:id', removeLike);
+router.put('/addlike/post/:id', isAuthenticated, addLike);
+router.put('/removelike/post/:id', isAuthenticated, removeLike);
 
 module.exports=router;
