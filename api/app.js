2e7c2071ba8aa58d412fa77926696a3c25c4355b
Roshan Das <91432324+RD2502@users.noreply.github.com>
2023-08-05 01:40:21 +0530

    Update app.js

--- api/app.js
+++ api/app.js
@@ -16,6 +16,7 @@ app.use((req, res, next) => {
 res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Authorization')
 res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
 res.setHeader('Access-Control-Allow-Credentials', 'true')
+res.setHeader('Access-Control-Expose-Headers','X-My-Custom-Header, Set-Cookie')
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
